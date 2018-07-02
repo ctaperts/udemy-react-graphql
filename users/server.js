871168d940f9f05ac5f7d15be36b7be77fa627c2
Colby Taperts <colbytaperts@gmail.com>
2018-07-01 20:22:14 -0500

    init graphql

---
+++ users/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const expressGraphQL = require('express-graphql');
+const schema = require('./schema/schema');
+
+const app = express();
+
+app.use('/graphql', expressGraphQL({
+  schema,
+  graphiql: true
+}));
+
+app.listen(3001, () => {
+  console.log('listening');
+});
