d28e1794b820738420c0f1e2df474f80b253d6b0
Colby Taperts <colbytaperts@gmail.com>
2018-07-01 23:12:30 -0500

    add axios/async

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -1,16 +1,11 @@
 const graphql = require('graphql');
-const _ = require('lodash');
+const axios = require('axios');
 const {
   GraphQLObjectType,
   GraphQLString,
   GraphQLInt,
   GraphQLSchema
 } = graphql;
-
-const users = [
-  { id: '23', firstName: "bill", age: 21 },
-  { id: '28', firstName: "alice", age: 22 }
-];
 
 const UserType = new GraphQLObjectType({
   name: 'User',
@@ -28,7 +23,8 @@ const RootQuery = new GraphQLObjectType({
       type: UserType,
       args: { id: { type: GraphQLString } },
       resolve(parentValue, args) {
-        return _.find(users, { id: args.id });
+        return axios.get(`http://localhost:3002/users/${args.id}`)
+          .then(res => res.data);
       }
     }
   }
